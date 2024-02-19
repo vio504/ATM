20a9cc81ddd1ca5b25b819f704382d189d67d896
Danial Shahla <160122649+vio504@users.noreply.github.com>
2024-02-19 17:05:31 +0100

    #include :(

--- proj++/Withdrawal.h
+++ proj++/Withdrawal.h
@@ -4,9 +4,27 @@
 #ifndef WITHDRAWAL_H
 #define WITHDRAWAL_H
 
+// forward declaration 
+class Screen;
+class keypad;
+class CashDispenser;
+class BankDatabase;
+
 class Withdrawal
 {
+public:
+    // operations
+    void execute(); // perform the transaction
+private:
+    // attributes
+    int accountNumber; // account to withdraw funds from
+    double amount; // amount to withdraw
 
+    // references to associated objects
+    Screen &screen;
+    keypad &keypad;
+    CashDispenser &cashdispenser;
+    BankDatabase &bankdatabase;
 };
 
-#endif 
+#endif // WITHDRAWAL_H
