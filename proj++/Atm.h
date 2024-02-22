42f16aba6a6642d1ddab7dd925da7f3576ca0456
Danial Shahla <160122649+vio504@users.noreply.github.com>
2024-02-22 19:40:58 +0100

    Definition of class ATM

---
+++ proj++/Atm.h
@@ -0,0 +1,36 @@
+// Atm.h
+// ATM class definition. Represents an automated teller machine.
+#ifndef ATM_H
+#define ATM_H
+
+#include "Screen.h"
+#include "Keybad.h"
+#include "CashDispenser.h" // class definition
+#include "DepositSlot.h"
+#include "BankDatabase.h"
+class Transaction;
+
+class ATM
+{
+public:
+    ATM();
+    void run(); // start the ATM
+private:
+    bool userAuthenticated;
+    int currentAcountNumber;
+    Screen screen;
+    Keypad keypad;
+    CashDispenser cashDispenser;
+    DepositSlot depositSlot;
+    BankDatabase bandDatabase;
+
+    // private utility functions
+    void authenticateUser();
+    void performTransactions();
+    int displayMainMenu() const;
+
+    // return object of specified Transaction derived class
+    Transaction *createTransaction(int);
+};
+
+#endif // ATM_H
