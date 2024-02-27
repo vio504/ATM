640357404c395b8a8d2d201291c14fca4bc0514b
Danial Shahla <160122649+vio504@users.noreply.github.com>
2024-02-27 16:59:13 +0100

    Keypad class definition

---
+++ proj++/Keyapad.h
@@ -0,0 +1,12 @@
+// Keypad.h
+// Keypad class definition . Represents the Keypad of the ATM.
+#ifndef KEYPAD_H
+#define KEYPAD_H
+
+class Keypad
+{
+public:
+    int getInput() const; // return an integer value entered by user
+};
+
+#endif
